31fe8b7917a9772a71e3ea2b24ea091df63be6e9
Owen Shepherd <owen.shepherd@obsidian.systems>
2020-01-29 00:19:47 +0000

    add alternative local files

--- source-local-shellrc.sh
+++ source-local-shellrc.sh
@@ -3,17 +3,19 @@
 # Contribute at https://github.com/414owen/local-shellrc
 # ---
 
-LOCAL_SHELL_FILE=".local.sh"
+LOCAL_SHELL_FILES=".local.sh ${1}"
 LOCAL_SHELL_FILE_LIMIT="${HOME}"
 
 # Taken from https://stackoverflow.com/a/18558871/6522680
 beginswith() { case $2 in "$1"*) true;; *) false;; esac; }
 
-# Search parent directories until we find a $LOCAL_SHELL_FILE, then source it
+# Search parent directories until we find a file in $LOCAL_SHELL_FILES, then source it
 source_local_shellrc() {
   local curr="$(pwd -P)"
-  while beginswith "${LOCAL_SHELL_FILE_LIMIT}" "${curr}" && [ "${curr}" != "${LOCAL_SHELL_FILE_LIMIT}" ] && [ "${curr}" != "/" ]; do
-    [ -f "${curr}/${LOCAL_SHELL_FILE}" ] && . "${curr}/${LOCAL_SHELL_FILE}"
+  while beginswith "${LOCAL_SHELL_FILE_LIMIT}" "${curr}" && [ "${curr}" != "${LOCAL_SHELL_FILE_LIMIT}" ]; do
+    for fname in $(echo "${LOCAL_SHELL_FILES}"); do
+      [ -f "${curr}/${fname}" ] && . "${curr}/${fname}"
+    done
     local curr="$(realpath "${curr}/..")"
   done
 }
